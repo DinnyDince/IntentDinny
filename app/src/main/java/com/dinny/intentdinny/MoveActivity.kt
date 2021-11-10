5cc954fcc97a57ef932f526edc2e66db112c6f9a
Dinny Wahyu Widarti <dinnywidarti@gmail.com>
2021-11-10 11:12:02 +0700

    lanjutan intent

---
+++ app/src/main/java/com/dinny/intentdinny/MoveActivity.kt
@@ -0,0 +1,11 @@
+package com.dinny.intentdinny
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
